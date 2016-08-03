1d43aaadc9341dfda881cfdfb75af2b5368e12c1
Daniele Candotti <daniele.candotti@hotmail.it>
2016-08-03 13:50:16 +0200

    Initial Commit

---
+++ Gauge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gauge
+//
+//  Created by Daniele Candotti on 03/08/2016.
+//  Copyright © 2016 Daniele Candotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
